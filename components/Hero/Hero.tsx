249bcf53aa798ff2ffb1c55e9daded611a69b966
Prajwal Dhungana <prajwaldhungana164@gmail.com>
2022-12-01 22:01:53 +0545

    style: add transitions and styles

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -3,76 +3,94 @@ import { motion } from "framer-motion";
 
 type Props = {};
 
+const hero = {
+  animate: {
+    transition: {
+      delayChildren: 0.2,
+      staggerChildren: 0.1,
+    },
+  },
+};
+
+const letterAnimation = {
+  initial: {
+    y: 400,
+  },
+  animate: {
+    y: 0,
+    transition: {
+      ease: [0.6, 0.01, -0.05, 0.95],
+      duration: 0.5,
+    },
+  },
+};
+
 export default function Hero({}: Props) {
   return (
     <div className="flex p-5 items-center h-full">
       <div className="p-6 ml-24">
-        <motion.div
-          initial={{
-            x: 50,
-            opacity: 0,
-            scale: 0.5,
-          }}
-          animate={{
-            x: 0,
-            opacity: 1,
-            scale: 1,
-          }}
-          transition={{
-            type: "spring",
-            stiffness: 400,
-            damping: 30,
-            delay: 0.6,
-          }}
-          className="text-6xl text-deep font-extrabold mb-2 font-heading uppercase tracking-wide overflow-hidden"
-        >
-          Prajwal Dhungana
+        <motion.div variants={hero}>
+          <HeroTitle title={"Prajwal Dhungana"} />
+          <HeroSubTitle
+            title={"Front-End Web Developer"}
+            transition={{ delay: 2 }}
+          />
+          <div className="overflow-hidden">
+            <motion.div
+              initial={{
+                x: -300,
+              }}
+              animate={{ x: 0 }}
+              transition={{
+                delay: 2,
+                duration: 1,
+              }}
+            >
+              <button className="btn">
+                <span className="btn-text">Know More About Me</span>
+              </button>
+            </motion.div>
+          </div>
         </motion.div>
-        <motion.div
-          initial={{
-            x: 50,
-            opacity: 0,
-            scale: 0.5,
-          }}
-          animate={{
-            x: 0,
-            opacity: 1,
-            scale: 1,
-          }}
-          transition={{
-            type: "spring",
-            stiffness: 400,
-            damping: 30,
-            delay: 0.7,
-          }}
-          className="text-2xl font-bold text-grey mb-5"
-        >
-          Front-End Web Developer
-        </motion.div>
-        <motion.button
-          initial={{
-            x: -500,
-            opacity: 0,
-            scale: 0.5,
-          }}
-          animate={{
-            x: 0,
-            opacity: 1,
-            scale: 1,
-            transition: {
-              duration: 1
-            }
-          }}
-          transition={{
-            type: "spring",
-            stiffness: 400,
-            damping: 30
-          }}
-          className="btn"
-        >
-          <span className="btn-text">Know More About Me</span>
-        </motion.button>
       </div>
     </div>
   );
 }
+
+const AnimatedLetters = ({ title, transition }: any) => {
+  return (
+    <motion.span
+      className="whitespace-pre-wrap"
+      variants={hero}
+      initial="initial"
+      animate="animate"
+      transition={transition}
+    >
+      {[...title].map((letter) => (
+        <motion.span
+          className="inline-block"
+          variants={letterAnimation}
+          key={Math.random()}
+        >
+          {letter}
+        </motion.span>
+      ))}
+    </motion.span>
+  );
+};
+
+const HeroTitle = ({ title }: any) => {
+  return (
+    <div className="overflow-hidden text-7xl font-heading mb-3 uppercase font-bold">
+      <AnimatedLetters title={title} />
+    </div>
+  );
+};
+
+const HeroSubTitle = ({ title, transition }: any) => {
+  return (
+    <div className="overflow-hidden text-3xl font-semibold text-grey mb-5">
+      <AnimatedLetters title={title} transition={transition} />
+    </div>
+  );
+};
