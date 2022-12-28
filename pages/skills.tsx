6a13e8517bc9bcd806d61922a7fa124aa1f3816b
Prajwal Dhungana <prajwaldhungana164@gmail.com>
2022-12-28 23:40:08 +0545

    refactor: Add full width div and remove span

--- pages/skills.tsx
+++ pages/skills.tsx
@@ -2,13 +2,15 @@ import Head from "next/head";
 
 export default function Skills() {
   return (
-      <div className="overflow-hidden">
+      <div className="overflow-hidden w-full">
         <div className="flex flex-col h-screen">
           <Head>
             <title>Portfolio - Skills</title>
           </Head>
-          <div>
-            <span className="text-2xl">Skills</span>
+          <div className="w-full p-5 items-center h-full">
+            <div className="text-4xl uppercase font-bold mb-5 px-3 py-20 text-charcoal text-center">
+              Skills
+            </div>
           </div>
         </div>
       </div>
