69722236b2cd076e8994e7016fa7e752de07fda2
Prajwal Dhungana <prajwaldhungana164@gmail.com>
2023-10-10 21:56:17 +0545

    deps: Add the missing dependency
    refactor: Minor changes

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -7,7 +7,7 @@ export default function Contact() {
   }
 
   return (
-      <div className="overflow-hidden w-full">
+      <div className="overflow-hidden">
         <div className="flex flex-col h-screen">
           <Head>
             <title>Portfolio - Contact</title>
--- pages/skills.tsx
+++ pages/skills.tsx
@@ -2,15 +2,13 @@ import Head from "next/head";
 
 export default function Skills() {
   return (
-      <div className="overflow-hidden w-full">
+      <div className="overflow-hidden">
         <div className="flex flex-col h-screen">
           <Head>
             <title>Portfolio - Skills</title>
           </Head>
-          <div className="w-full p-5 items-center h-full">
-            <div className="text-4xl uppercase font-bold mb-5 px-3 py-20 text-charcoal text-center">
-              Skills
-            </div>
+          <div>
+            <span className="text-2xl">Skills</span>
           </div>
         </div>
       </div>
