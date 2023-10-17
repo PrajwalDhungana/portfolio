c5188aebe6f4b753c6a60669510cbae3721efe1f
Prajwal Dhungana <prajwaldhungana164@gmail.com>
2023-10-18 02:31:30 +0545

    feat: Define Props type
    refactor: Rename variants to make them relevant
    
    style: Update on variants
    
    feat: Decouple components

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -1,10 +1,24 @@
-import React, { useState, useEffect } from "react";
-import { motion } from "framer-motion";
-import Link from "next/link";
+import React from "react";
+import { Variants, motion } from "framer-motion";
+import AnimatedLetters from "../utils/AnimatedLetters";
+import Heading from "../utils/Heading";
 
-type Props = {};
+type Props = {
+  title: string;
+  classes?: string;
+  duration?: number;
+  delay?: number;
+  letterDelay?: Variants;
+};
 
-const hero = {
+/**
+ * Variant to slide individual alphabet up
+ * Every children is staggered with a delay
+ *
+ * @since 1.0.0
+ * @returns object
+ */
+export const letterDelay = {
   animate: {
     transition: {
       delayChildren: 0.1,
@@ -13,7 +27,13 @@ const hero = {
   },
 };
 
-const letterAnimation = {
+/**
+ * Variant to slide individual alphabet up
+ *
+ * @since 1.0.0
+ * @returns object
+ */
+export const letterAnimation = {
   initial: {
     y: 400,
   },
@@ -21,7 +41,8 @@ const letterAnimation = {
     y: 0,
     transition: {
       ease: [0.6, 0.01, -0.05, 0.95],
-      duration: 1,
+      duration: 2,
+      delay: 2,
     },
   },
 };
@@ -29,83 +50,98 @@ const letterAnimation = {
 export default function Hero({}: Props) {
   return (
     <div className="flex items-center h-full flex-1 justify-center">
-      <div className="p-6 -mt-32">
-        <motion.div variants={hero} className="text-center">
-          <HeroTitle title={"Prajwal"} />
-          <HeroSubTitle
-            title={"Versatile React developer crafting seamless and interactive web applications."}
-            transition={{ delay: 2 }}
-          />
-          {/* <div className="overflow-hidden">
-            <motion.div
-              initial={{
-                y: -300,
+      <div className="absolute text-[256px] font-bold opacity-5 left-[47%] top-40 truncate text-clip max-w-[53%]">
+        <AnimatedLetters
+          title={["IMAGINATION"]}
+          letterAnimation={{
+            initial: {
+              x: -800,
+              opacity: 0,
+              transition: {
+                delay: 1,
+              },
+            },
+            animate: {
+              x: 0,
+              opacity: 1,
+              transition: {
+                ease: [0.6, 1, 0.3, 1],
+                duration: 2,
+                delay: 1,
+              },
+            },
+          }}
+        />
+      </div>
+      <div className="p-6 w-full max-w-[860px]">
+        <motion.div variants={letterDelay}>
+          <div className="overflow-hidden text-3xl md:text-5xl lg:text-8xl font-heading mb-5 uppercase font-bold">
+            <Heading
+              title={`Unlocking`}
+              classes={`font-normal text-3xl tracking-[1rem] mb-6 ml-1.5`}
+              delay={0.5}
+            />
+            <AnimatedLetters
+              title={["IMAGINATION"]}
+              letterAnimation={{
+                initial: {
+                  x: -800,
+                  opacity: 0,
+                  transition: {
+                    delay: 1.15,
+                  },
+                },
+                animate: {
+                  x: 0,
+                  opacity: 1,
+                  transition: {
+                    ease: [0.6, 1, 0.3, 1],
+                    duration: 2,
+                    delay: 1.15,
+                  },
+                },
               }}
-              animate={{ y: 0 }}
-              transition={{
-                delay: 0.5,
-                duration: 1,
-              }}
-            >
-              <Link href="/about">
-                <button className="bg-clay py-5 px-5 hover:bg-charcoal text-white">
-                  <span className="text-sm lg:text-xl font-bold">Know More About Me</span>
-                </button>
-              </Link>
-            </motion.div>
-          </div> */}
+              letterDelay={letterDelay}
+            />
+            <Heading
+              title={`Through`}
+              classes={`font-normal text-3xl tracking-[1rem] mb-6 ml-1.5 mt-12 text-end`}
+              delay={2}
+            />
+            <AnimatedLetters
+              title={["DESIGN"]}
+              classes={`text-end text-red-600`}
+              letterAnimation={letterAnimation}
+              letterDelay={letterDelay}
+            />
+          </div>
         </motion.div>
+      </div>
+      <div className="absolute text-end text-[256px] font-bold opacity-5 right-[57%] bottom-0 truncate text-clip">
+        <AnimatedLetters
+          title={["IMAGINATION"]}
+          letterAnimation={{
+            initial: {
+              x: -800,
+              opacity: 0,
+              transition: {
+                delay: 0,
+              },
+            },
+            animate: {
+              x: 0,
+              opacity: 1,
+              transition: {
+                ease: [0.6, 1, 0.3, 1],
+                duration: 2,
+                delay: 1,
+              },
+            },
+          }}
+          transition={{}}
+          classes={``}
+        />
       </div>
     </div>
   );
 }
-
-const AnimatedLetters = ({ title, transition }: any) => {
-  return (
-    <motion.span
-      className="whitespace-pre-wrap"
-      variants={hero}
-      initial="initial"
-      animate="animate"
-      transition={transition}
-    >
-      {[...title].map((letter) => (
-        <motion.span
-          className="inline-block"
-          variants={letterAnimation}
-          key={Math.random()}
-        >
-          {letter}
-        </motion.span>
-      ))}
-    </motion.span>
-  );
-};
-
-const HeroTitle = ({ title }: any) => {
-  return (
-    <div className="overflow-hidden text-3xl md:text-5xl lg:text-7xl font-heading mb-5 uppercase font-bold">
-      <p className="inline-block mr-6">{`Hi! I'm`}</p>
-      <AnimatedLetters title={title} />
-    </div>
-  );
-};
-
-const HeroSubTitle = ({ title, transition }: any) => {
-  const [slideUp, setSlideUp] = useState("");
-  useEffect(() => {
-    setTimeout(() => {
-      setSlideUp("slide-up")
-    }, 1500)
-    return () => {
-    }
-  }, [slideUp])
-
-  return (
-    <div className="overflow-hidden text-lg md:text-xl lg:text-3xl font-istokWeb text-charcoal mb-8 max-w-[650px]">
-      {/* <AnimatedLetters title={title} transition={transition} /> */}
-      <p className={`sub-title ${slideUp} transition-transform duration-[1.5s]`}>Versatile React developer crafting seamless and</p>
-      <p className={`sub-title ${slideUp}  transition-transform duration-[1.5s] delay-200`}> interactive web applications.</p>
-    </div>
-  );
-};
